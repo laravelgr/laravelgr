7ac3a451edaed3739dc011de8564fb780242f337
GregKos <gregkos.dev@gmail.com>
2025-05-13 23:23:47 +0300

    Font that supports both languages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ __('homepage.title') }}</title>
     <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     @vite('resources/css/app.css')
     <style>
         body {
