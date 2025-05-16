f0b6221416375f42c21363e9d8f38dfbb9f6d1c6
Drakakis George <drakakisgeo@users.noreply.github.com>
2025-05-16 17:34:45 +0300

    Ten year short link

--- config/shortcuts.php
+++ config/shortcuts.php
@@ -2,11 +2,12 @@
 
 /**
  * Warning, url is like this for example -> laravel.gr/s/subscribe
- * 
+ *
  */
 return [
     'urls' => [
         'subscribe' => 'https://mailchi.mp/f98a09aa59f4/laravel-community-sync',
         'thess' => 'https://lu.ma/qrdc9ibi',
+        '10-years' => 'https://lu.ma/3nkfpq8q'
     ],
 ];
